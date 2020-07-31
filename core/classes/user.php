318c6da8ecbb3d4bfb54226b5e6464c215e8d2f3
Muhammad Nur Ali <muhammadnurali303@gmail.com>
2020-08-01 00:10:30 +0700

    Fixed user login display

--- core/classes/user.php
+++ core/classes/user.php
@@ -1,10 +1,19 @@
-<?php 
-    class User {
-        protected $pdo;
+<?php
+class User
+{
+    protected $pdo;
 
-        function __construct($pdo)
-        {   
-            $this->pdo = $pdo;
-        }
+    function __construct($pdo)
+    {
+        $this->pdo = $pdo;
     }
-?>
+
+    public function userData($user_id)
+    {
+        // $stmt = $this->pdo->prepare("SELECT * FROM `users` WHERE `user_id` = :user_id");
+        $stmt = $this->pdo->prepare("SELECT * FROM `users` WHERE `user_id` = :user_id");
+        $stmt->bindParam(":user_id", $user_id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_OBJ);
+    }
+}
